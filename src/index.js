85a78c44596f58af72b64556125c7535e20ec987
= <chechenyshka@mail.ru>
2017-09-22 17:40:36 +0300

    there is a count of zeros for the usual factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function zeros(expression) {
-  // your solution
+    let numbers = expression.split('*');
+    let zeros = 0;
+    let multiplier;
+    let velue;
+    for (let i = 0; i < numbers.length; i++) {
+        velue = numbers[i];
+
+        if (velue[velue.length - 2] === '!') {
+            velue = parseInt(velue, 10);
+            if (veluse % 2 == 0) {
+                multiplier = 2;
+            }
+
+
+        } else {
+            velue = parseInt(velue, 10);
+            multiplier = 5;
+            while (true) {
+                if (velue >= multiplier) {
+                    zeros += Math.floor(velue / multiplier);
+                    multiplier *= 5;
+                } else {
+                    break;
+                }
+            }
+
+
+        }
+    }
+    return zeros;
+    // console.log(e);
 }
